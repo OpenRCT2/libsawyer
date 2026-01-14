a5d6fa6ca5222782d135485085f49f2cdd70730a
Aaron van Geffen <aaron@aaronweb.net>
2026-01-14 22:04:24 +0100

    Drop breakpad, discord-rpc, duktape, dukglue

--- src/Scripting.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "thirdparty/dukglue/dukglue.h"
-
-namespace cs::scripting
-{
-    void addPolyfillPromise(duk_context* ctx);
-}
--- test/TestData.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <string>
-#include <string_view>
-
-std::string readTestData(std::string_view fileName);
